82606755aee2936009ef7ff976c0c89eabc75afc
Krish Barot <HP@mac.home>
2025-11-05 23:35:46 -0500

    DB and Tools setup

---
+++ backend/src/lib/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+export const connectDB = async() => {
+    try {
+        if(!process.env.MONGODB_URI){
+            console.log("The environment variable is not set up for database")
+        }
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("connected to MongoDB")
+    } catch (error) {
+        console.error("Error connecting to MongoDB")
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,22 @@
 import express from "express"
 import dotenv from "dotenv"
+import { connectDB } from "./lib/db.js";
 
 dotenv.config()
 
 const app = express()
 
 app.get("/health", (req, res) => {
-    res.status(200).json({message: "API is running!"})
+    res.status(200).json({ message: "API is running!" })
 });
 
-app.listen(process.env.PORT, (req, res) => {
-    console.log(`Server running on port ${process.env.PORT}`)
-})
+const startServer = async () => {
+    try {
+        await connectDB();
+        console.log(`Server running on port ${process.env.PORT}`)
+    } catch (error) {
+        console.error("Error starting the server -> ", error)
+    }
+}
+
+startServer();
