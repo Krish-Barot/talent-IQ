f8a5f830e5102933d33600eeac2c50f11a3eaf5a
Krish Barot <HP@mac.home>
2025-11-07 23:24:24 -0500

    Landing Page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
 import { Toaster } from 'react-hot-toast'
-import { Routes } from 'react-router'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import { useUser } from '@clerk/clerk-react'
+import DashboardPage from './pages/DashboardPage';
 
 function App() {
+  const { isSignedIn, isLoaded } = useUser();
 
+  // To get rid of flickering section
+  if(!isLoaded){
+    return null;
+  }
   return (
+
     <>
       <Routes>
-        <h1>Welcom To The App</h1>
-
+        <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
       </Routes>
 
       <Toaster />
