19addeefcdbda7a9d675a437a73d9e9043ba04b6
Krish Barot <HP@mac.home>
2025-11-11 21:47:26 -0500

    Added problem details page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage'
 import { useUser } from '@clerk/clerk-react'
 import DashboardPage from './pages/DashboardPage';
 import ProblemsPage from './pages/ProblemsPage';
+import ProblemPage from './pages/ProblemPage';
 
 function App() {
   const { isSignedIn, isLoaded } = useUser();
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
         <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
       </Routes>
 
       <Toaster />
