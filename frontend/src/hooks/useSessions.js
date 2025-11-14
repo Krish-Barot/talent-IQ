77f0a39adc370d44b05d49a972e2894452790393
Krish Barot <HP@mac.home>
2025-11-13 21:11:13 -0500

    Added Dashboard Page

---
+++ frontend/src/hooks/useSessions.js
@@ -0,0 +1,61 @@
+import { useQuery, useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { sessionApi } from "../api/session.js";
+
+export const useCreateSession = () => {
+    const result = useMutation({
+        mutationKey: ["createSession"],
+        mutationFn: sessionApi.createSession,
+        onSuccess: () => toast.success("Session created Successfully !"),
+        onError: (error) => toast.error(error.response?.data?.message || "Failed to create session !")
+    });
+
+    return result;
+};
+
+export const useActiveSessions = () => {
+    const result = useQuery({
+        queryKey: ["activeSessions"],
+        queryFn: sessionApi.getActiveSession
+    });
+
+    return result;
+};
+
+export const useMyRecentSessions = () => {
+    const result = useQuery({
+        queryKey: ["myRecentSessions"],
+        queryFn: sessionApi.getMyRecentSessions
+    });
+
+    return result;
+};
+
+export const useSessionById = (id) => {
+    const result = useQuery({
+        queryKey: ["session", id],
+        queryFn: () => sessionApi.getSessionById(id),
+        enabled: !!id,
+        refetchInterval: 5000 // refetch every 5 seconds to detect session status changes
+    });
+
+    return result;
+};
+
+export const useJoinSession = (id) => {
+    return useMutation({
+        mutationKey: ["joinSession"],
+        mutationFn: () => sessionApi.joinSession(id),
+        onSuccess: () => toast.success("Join session successfully !"),
+        onError: (error) => toast.error(error.response?.data?.message || "Failed to join session !")
+    })
+};
+
+export const useEndSession = (id) => {
+    return useMutation({
+        mutationKey: ["endSession"],
+        mutationFn: () => sessionApi.endSession(id),
+        onSuccess: () => toast.success("Session ended successfully !"),
+        onError: (error) => toast.error(error.response?.data?.message || "Failed to end session !")
+    })
+};
